f585017a573b1d8e9e392951afb622eda5f027af
Rafayj34 <rafayj34@gmail.com>
2023-07-26 17:23:16 +0500

    Second commit. Added some animations and transitions.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,10 @@
 import { Suspense,useEffect,useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls,Preload, meshBounds, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader';
 
-const Computers = () => {
+import  CanvasLoader  from '../Loader'
+const Computers = ({ isMobile }) => {
+  // Const Computers =
   const computer = useGLTF('./desktop_pc/scene.gltf');
 
   return (
@@ -12,7 +13,7 @@ const Computers = () => {
       groundColor="black"/>
       <pointLight intensity={1}/>
       <spotLight
-      position={[-20,50,10]}
+      position={isMobile ? [0,-3,-2.2] : [-20,50,10]}
       angle={0.12}
       penumbra={1}
       intensity={1}
@@ -21,7 +22,7 @@ const Computers = () => {
       />
       <primitive 
       object= {computer.scene}
-      scale={0.75}
+      scale={isMobile ? 0.7 : 0.75}
       position={[0,-3.25,-1.5]}
       rotation={[-0.01,-0.2,-0.1]}
       />
@@ -29,6 +30,24 @@ const Computers = () => {
   )
 }
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect (() => {
+    //listener to check screensize
+    const mediaQuery = window.matchMedia('(max-width:500px)');
+
+    setIsMobile(mediaQuery.matches); //to set its initial value
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener('change',handleMediaQueryChange);
+    }
+     
+},[])
   return (
     <Canvas
     frameloop='demand'
@@ -36,11 +55,11 @@ const ComputersCanvas = () => {
     camera={{position: [20,3,5], fov:25}}
     gl={{preserveDrawingBuffer: true}}
     >
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false}
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}/>
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
